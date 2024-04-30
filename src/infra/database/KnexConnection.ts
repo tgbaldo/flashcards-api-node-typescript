217457d49376b59dc3c4df96fff4f0e27b78a3e1
Tiago Baldo <tgbaldo@gmail.com>
2024-04-29 22:37:30 -0300

    knex stat

--- src/infra/database/KnexConnection.ts
+++ src/infra/database/KnexConnection.ts
@@ -1,1 +1,12 @@
-//
+import knex from "knex";
+
+export default class KnexConnection {
+  connect() {
+    return knex({
+      client: 'sqlite3',
+      connection: {
+        filename: './database.sqlite',
+      },
+    });
+  }
+}
